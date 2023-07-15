e4e6e10dad587e5d4140b94eb55e6659211d0cc1
Valery <example@gmail.com>
2023-07-15 16:03:21 +0100

    add practice

--- js/objects/practice.js
+++ js/objects/practice.js
@@ -98,3 +98,57 @@ const personalMovieDB = {
     }
   },
 };
+// ////////////////////////////////////////////////////////////////////
+const shoppingMallData = {
+  shops: [
+    {
+      width: 10,
+      length: 5,
+    },
+    {
+      width: 15,
+      length: 7,
+    },
+    {
+      width: 20,
+      length: 5,
+    },
+    {
+      width: 8,
+      length: 10,
+    },
+  ],
+  height: 5,
+  moneyPer1m3: 30,
+  budget: 50000,
+};
+
+function isBudgetEnough(data) {
+  let square = 0;
+  let volume = 0;
+
+  data.shops.forEach((shop) => {
+    square += shop.width * shop.length;
+  });
+
+  volume = data.height * square;
+
+  if (data.budget - volume * data.moneyPer1m3 >= 0) {
+    return "Бюджета достаточно";
+  } else {
+    return "Бюджета недостаточно";
+  }
+  // const totalArea = data.shops.reduce((acc, shop) => {
+  //   const shopsArea = shop.width * shop.length;
+  //   return acc + shopsArea;
+  // }, 0);
+  // const volume = totalArea * data.height;
+  // const checkBudget = volume * data.moneyPer1m3;
+  // if (data.budget - checkBudget >= 0) {
+  //   return "Бюджета недостаточно!";
+  // } else {
+  //   return "Бюджета достаточно";
+  // }
+}
+
+console.log(isBudgetEnough(shoppingMallData));
--- js/practice/codwars.js
+++ js/practice/codwars.js
@@ -6,7 +6,7 @@ function solution(str, ending) {
   //   const strNew = str.slice(-l);
   //   return strNew.match(ending) ? true : false;
 }
-console.log(solution("abc", "bc"));
+// console.log(solution("abc", "bc"));
 
 // Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
 
@@ -21,7 +21,7 @@ function saleHotdogs(n) {
     return n * 90;
   }
 }
-console.log(saleHotdogs(10));
+// console.log(saleHotdogs(10));
 
 //Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
 function greet(name, owner) {
@@ -32,8 +32,8 @@ function greet(name, owner) {
   }
   // return name === owner ? 'Hello boss' : 	'Hello guest';
 }
-console.log(greet("Daniel", "Daniel"));
-console.log(greet("Greg", "Daniel"));
+// console.log(greet("Daniel", "Daniel"));
+// console.log(greet("Greg", "Daniel"));
 
 // You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
 // If it is a square, return its area. If it is a rectangle, return its perimeter.
@@ -45,8 +45,8 @@ const areaOrPerimeter = function (l, w) {
   // return l == w ? l*w : 2*(l + w)
 };
 
-console.log(areaOrPerimeter(3, 3));
-console.log(areaOrPerimeter(6, 10));
+// console.log(areaOrPerimeter(3, 3));
+// console.log(areaOrPerimeter(6, 10));
 
 // Code as fast as you can! You need to double the integer and return it.
 function doubleInteger(i) {
@@ -54,7 +54,7 @@ function doubleInteger(i) {
     return i * 2;
   }
 }
-console.log(doubleInteger(2));
+// console.log(doubleInteger(2));
 
 // Your task is to create a function that does four basic mathematical operations.
 
@@ -74,7 +74,18 @@ function basicOp(operation, value1, value2) {
       return null;
   }
 }
-console.log(basicOp(("+", 4, 7)));
-console.log(basicOp(("-", 15, 18)));
-console.log(basicOp(("*", 5, 5)));
-console.log(basicOp(("/", 49, 7)));
+// console.log(basicOp(("+", 4, 7)));
+// console.log(basicOp(("-", 15, 18)));
+// console.log(basicOp(("*", 5, 5)));
+// console.log(basicOp(("/", 49, 7)));
+
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+var summation = function (num) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+  // return num * (num + 1) / 2
+};
+console.log(summation(2));
