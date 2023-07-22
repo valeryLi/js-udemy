47e141472c0e6ecaa1908a54f457a2a40221c9dd
Valery <example@gmail.com>
2023-07-22 17:07:53 +0100

    add jpg

--- js/objects/practice.js
+++ js/objects/practice.js
@@ -152,3 +152,55 @@ function isBudgetEnough(data) {
 }
 
 console.log(isBudgetEnough(shoppingMallData));
+
+// /////////////////////////////////////////////////////////////////////////////
+const students = [
+  "Peter",
+  "Andrew",
+  "Ann",
+  "Mark",
+  "Josh",
+  "Sandra",
+  "Cris",
+  "Bernard",
+  "Takesi",
+  "Sam",
+];
+
+function sortStudentsByGroups(arr) {
+  arr.sort();
+  const a = [],
+    b = [],
+    c = [],
+    rest = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i < 3) {
+      a.push(arr[i]);
+    } else if (i < 6) {
+      b.push(arr[i]);
+    } else if (i < 9) {
+      c.push(arr[i]);
+    } else {
+      rest.push(arr[i]);
+    }
+  }
+  return [
+    a,
+    b,
+    c,
+    `Оставшиеся студенты: ${rest.length === 0 ? "-" : rest.join(", ")}`,
+  ];
+
+  // console.log(sortedStudents);
+
+  // let newArr = [];
+  // let studentGroup = 3;
+
+  // for (let i = 0; i < sortedStudents.length; i += studentGroup) {
+  //   newArr.push(sortedStudents.slice(i, i + studentGroup));
+  // }
+  // console.log(newArr);
+}
+
+console.log(sortStudentsByGroups(students));
--- js/practice/codwars.js
+++ js/practice/codwars.js
@@ -6,7 +6,7 @@ function solution(str, ending) {
   //   const strNew = str.slice(-l);
   //   return strNew.match(ending) ? true : false;
 }
-// console.log(solution("abc", "bc"));
+console.log(solution("abc", "bc"));
 
 // Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
 
@@ -21,7 +21,7 @@ function saleHotdogs(n) {
     return n * 90;
   }
 }
-// console.log(saleHotdogs(10));
+console.log(saleHotdogs(10));
 
 //Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
 function greet(name, owner) {
@@ -32,7 +32,7 @@ function greet(name, owner) {
   }
   // return name === owner ? 'Hello boss' : 	'Hello guest';
 }
-// console.log(greet("Daniel", "Daniel"));
+console.log(greet("Daniel", "Daniel"));
 // console.log(greet("Greg", "Daniel"));
 
 // You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
@@ -45,7 +45,7 @@ const areaOrPerimeter = function (l, w) {
   // return l == w ? l*w : 2*(l + w)
 };
 
-// console.log(areaOrPerimeter(3, 3));
+console.log(areaOrPerimeter(3, 3));
 // console.log(areaOrPerimeter(6, 10));
 
 // Code as fast as you can! You need to double the integer and return it.
@@ -54,7 +54,7 @@ function doubleInteger(i) {
     return i * 2;
   }
 }
-// console.log(doubleInteger(2));
+console.log(doubleInteger(2));
 
 // Your task is to create a function that does four basic mathematical operations.
 
@@ -74,7 +74,7 @@ function basicOp(operation, value1, value2) {
       return null;
   }
 }
-// console.log(basicOp(("+", 4, 7)));
+console.log(basicOp(("+", 4, 7)));
 // console.log(basicOp(("-", 15, 18)));
 // console.log(basicOp(("*", 5, 5)));
 // console.log(basicOp(("/", 49, 7)));
@@ -89,3 +89,88 @@ var summation = function (num) {
   // return num * (num + 1) / 2
 };
 console.log(summation(2));
+
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+var countSheep = function (num) {
+  let result = "";
+  for (let i = 1; i <= num; i++) {
+    result += i + " sheep...";
+  }
+  return result;
+};
+console.log(countSheep(2));
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+//Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+function paperwork(n, m) {
+  if (n < 0 || m < 0) {
+    return 0;
+  }
+  return n * m;
+  // return n > 0 && m > 0 ? n * m : 0;
+}
+console.log(paperwork(5, 5));
+console.log(paperwork(-5, -5));
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element
+function removeEveryOther(arr) {
+  // return arr.filter((item, index) => {
+  //   return index % 2 === 0;
+  // });
+  let result = [];
+  for (let i = 0; i < arr.length; i += 2) {
+    result.push(arr[i]);
+  }
+  return result;
+}
+console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"]));
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+function bmi(weight, height) {
+  const bodyIndex = Math.round(weight / Math.pow(height, 2));
+
+  if (bodyIndex <= 18.5) {
+    return "Underweight";
+  }
+  if (bodyIndex <= 25) {
+    return "Normal";
+  }
+  if (bodyIndex <= 30) {
+    return "Overweight";
+  }
+  if (bodyIndex > 30) {
+    return "Obese";
+  }
+}
+console.log(bmi(98.8, 1.76));
+console.log(bmi(80, 1.8));
+
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+function smash(words) {
+  return words.join(" ");
+}
+
+console.log(smash(["hello", "world"]));
+
+// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+function rentalCarCost(d) {
+  if (d < 3) {
+    return d * 40;
+  }
+  if (d >= 3 && d < 7) {
+    return d * 40 - 20;
+  }
+  if (d >= 7) {
+    return d * 40 - 50;
+  }
+  //  return d * 40 - (d >= 7 ? 50 : (d >= 3 ? 20 : 0));
+}
+console.log(rentalCarCost(10));
